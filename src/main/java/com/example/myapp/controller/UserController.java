ed7300a3c4b33b3df690efd4c99af19bf7e48449
faleevand <faleevandre@gmail.com>
2023-06-15 02:42:55 +0300

    Initial commit

---
+++ src/main/java/com/example/myapp/controller/UserController.java
@@ -0,0 +1,76 @@
+package com.myapp.sprbootapp.controller;
+
+
+import com.myapp.sprbootapp.model.User;
+import com.myapp.sprbootapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String allusers(Model model) {
+        model.addAttribute("users", userService.findAll());
+        return "/allusers";
+    }
+
+    @RequestMapping("users/{id}")
+    public String show(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("user", userService.findById(id));
+        return "/show";
+    }
+
+    @RequestMapping("/new")
+    public String newPerson(@ModelAttribute("user") User user) {
+        return "/new";
+    }
+
+    @RequestMapping("/users")
+    public String create(@Valid @ModelAttribute("user") User user,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "/new";
+
+        userService.saveUser(user);
+        return "redirect:/";
+    }
+
+    @RequestMapping("users/{id}/edit")
+    public String edit(Model model, @PathVariable("id") Long id) {
+        model.addAttribute("user", userService.findById(id));
+        return "/edit";
+    }
+
+    @RequestMapping("{id}/update")
+    public String update(@Valid @ModelAttribute("user") User user, BindingResult bindingResult,
+                         @PathVariable("id") long id) {
+        if (bindingResult.hasErrors())
+            return "/edit";
+
+        userService.saveUser(user);
+        return "redirect:/";
+    }
+
+    @RequestMapping("users/{id}/delete")
+    public String delete(@PathVariable("id") Long id) {
+        userService.deleteById(id);
+        return "redirect:/";
+    }
+
+
+}
---
+++ src/main/java/com/example/myapp/model/User.java
@@ -0,0 +1,34 @@
+package com.myapp.sprbootapp.model;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "One more time")
+    @Column(name = "name")
+    private String name;
+
+    @Min(value = 0, message = "One more time")
+    @Max(value = 130, message = "One more time")
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "surname")
+    @NotBlank(message = "One more time")
+    private String surname;
+
+}
